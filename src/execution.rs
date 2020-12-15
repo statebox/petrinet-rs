b8fc4e9d32fa7e181c6c639172e547045a863945
Robert Hambrock <roberthambrock@gmail.com>
2020-12-15 20:52:35 +0100

    Refactor with pattern matching

--- src/execution.rs
+++ src/execution.rs
@@ -29,31 +29,35 @@ impl<'a> Execution<'a> {
         }
     }
     pub fn fire(self, t: usize) -> Self {
-        if !self.enabled(t) {return self;}
-        let Execution(net, marking) = self;
-        let tr = net.transition(t).expect("already know transition is enabled and therefore exists");
-        let cs = tr.consume();
-        let ps = tr.produce();
+        match self.enabled(t) {
+            false => self,
+            true => {
+                let Execution(net, marking) = self;
+                let tr = net.transition(t).expect("already know transition is enabled and therefore exists");
+                let cs = tr.consume();
+                let ps = tr.produce();
 
-        let new_marking = marking
-            .iter()
-            .enumerate()
-            .map(|(pos, &has_token)| {
-                let place_ix = &(pos + 1);
-                let is_consumed = cs.contains(place_ix);
-                let is_produced = ps.contains(place_ix);
-                let yes_token = true; // net is 1-safe, so place either has or doesn't have a token
-                let no_token = false;
-                match (is_consumed, is_produced) {
-                    (true, true) => yes_token, // both produce and consumed, so there is a token afterwards
-                    (true, false) => no_token, // just consumed, so there is _no_ token afterwards
-                    (false, true) => yes_token, // only produced, so there is a token afterwards
-                    (false, false) => has_token, // not modified by this transition
-                }
-            })
-            .collect();
+                let new_marking = marking
+                    .iter()
+                    .enumerate()
+                    .map(|(pos, &has_token)| {
+                        let place_ix = &(pos + 1);
+                        let is_consumed = cs.contains(place_ix);
+                        let is_produced = ps.contains(place_ix);
+                        let yes_token = true; // net is 1-safe, so place either has or doesn't have a token
+                        let no_token = false;
+                        match (is_consumed, is_produced) {
+                            (true, true) => yes_token, // both produce and consumed, so there is a token afterwards
+                            (true, false) => no_token, // just consumed, so there is _no_ token afterwards
+                            (false, true) => yes_token, // only produced, so there is a token afterwards
+                            (false, false) => has_token, // not modified by this transition
+                        }
+                    })
+                    .collect();
 
-        // println!("execution: {:?}", new_marking);
-        Execution(net, new_marking)
+                // println!("execution: {:?}", new_marking);
+                Execution(net, new_marking)
+            }
+        }
     }
 }
